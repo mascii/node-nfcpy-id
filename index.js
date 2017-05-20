9518f565d698a6b9489e4738d1c0fa28a3f5dfd4
Masaki Koyanagi <mascii@gmail.com>
2017-05-21 02:38:36 +0900

    add stop() method

--- index.js
+++ index.js
@@ -10,21 +10,23 @@ module.exports = class NfcpyId extends EventEmitter {
                                 options.scriptFile : 'reader.py';
         const scriptPath = 'scriptPath' in (options || {}) ?
                                 options.scriptPath : __dirname;
-        const pyshell = new PythonShell(scriptFile, {scriptPath}, {mode: 'JSON'});
+        this.pyshell = new PythonShell(scriptFile, {scriptPath}, {mode: 'JSON'});
 
-        pyshell.stdout.on('data', (json) => {
+        this.pyshell.stdout.on('data', (json) => {
           const data = JSON.parse(json.split('\n')[0]);
           this.emit(data.event, data);
         });
 
-        pyshell.end((err) => {
+        this.pyshell.end((err) => {
           this.emit('error', err);
         });
 
         ['SIGHUP', 'SIGINT', 'exit'].forEach((event) => {
-          process.on(event, () => {
-            pyshell.childProcess.kill('SIGHUP');
-          });
+          process.on(event, () => {this.stop()});
         });
+    }
+
+    stop() {
+      this.pyshell.childProcess.kill('SIGHUP');
     }
 }
