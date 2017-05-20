4eb71690335cd61738bb0e5c2b0864c9ed20a033
Masaki Koyanagi <mascii@gmail.com>
2017-05-21 08:52:04 +0900

    add start() method

--- index.js
+++ index.js
@@ -5,25 +5,32 @@ const PythonShell = require('python-shell');
 
 module.exports = class extends EventEmitter {
     constructor(options) {
-        super();
-        const scriptFile = 'scriptFile' in (options || {}) ?
-                                options.scriptFile : 'reader.py';
-        const scriptPath = 'scriptPath' in (options || {}) ?
-                                options.scriptPath : __dirname;
-        this.pyshell = new PythonShell(scriptFile, {scriptPath}, {mode: 'JSON'});
+      super();
+      this.options = options;
+      this.start();
+    }
 
-        this.pyshell.stdout.on('data', (json) => {
-          const data = JSON.parse(json.split('\n')[0]);
-          this.emit(data.event, data);
-        });
+    start() {
+      const scriptFile = 'scriptFile' in (this.options || {}) ?
+                              this.options.scriptFile : 'reader.py';
+      const scriptPath = 'scriptPath' in (this.options || {}) ?
+                              this.options.scriptPath : __dirname;
+      this.pyshell = new PythonShell(scriptFile, {scriptPath}, {mode: 'JSON'});
 
-        this.pyshell.end((err) => {
-          this.emit('error', err);
-        });
+      this.pyshell.stdout.on('data', (json) => {
+        const data = JSON.parse(json.split('\n')[0]);
+        this.emit(data.event, data);
+      });
 
-        ['SIGHUP', 'SIGINT', 'exit'].forEach((event) => {
-          process.on(event, () => {this.stop()});
+      this.pyshell.end((err) => {
+        this.emit('error', err);
+      });
+
+      ['SIGHUP', 'SIGINT', 'exit'].forEach((event) => {
+        process.on(event, () => {
+          this.stop();
         });
+      });
     }
 
     stop() {
